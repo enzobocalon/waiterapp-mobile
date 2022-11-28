90988e3d208d249630c246f2f4d5e04d3055d4b2
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-11-28 13:28:40 -0300

    feat: button added

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from '../components/Button';
 import { Categories } from '../components/Categories';
 import { Header } from '../components/Header';
 import { Menu } from '../components/Menu';
@@ -21,7 +22,9 @@ export function Main() {
 
       <Footer>
         <FooterContainer>
-
+          <Button onPress={() => alert('teste')}>
+            Novo Pedido
+          </Button>
         </FooterContainer>
       </Footer>
     </>
