125cbe3221637938b442a5dc635325c82b732ba7
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-11-28 13:27:37 -0300

    refactor: change border radius

--- src/components/Categories/styles.ts
+++ src/components/Categories/styles.ts
@@ -12,7 +12,7 @@ export const Icon = styled.View`
   background-color: white;
   width: 44px;
   height: 44px;
-  border-radius: 50%;
+  border-radius: 22px;
   align-items: center;
   justify-content: center;
   margin-bottom: 8px;
