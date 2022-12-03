97096c65ce11fb90c506d0c00826c6140bc06504
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-12-03 16:04:38 -0300

    refactor: changed product to state

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { FlatList } from 'react-native';
-import { products } from '../../mocks/products';
 import { Product } from '../../types/Product';
 import { formatCurrency } from '../../utils/formatCurrency';
 import { PlusCircle } from '../Icons/PlusCircle';
@@ -11,9 +10,10 @@ import { ProductContainer, Image, ProductDetails, Separator, AddToCartButton } f
 
 interface MenuProps {
   onAddToCart: (product: Product) => void;
+  products: Product[]
 }
 
-export function Menu({onAddToCart}: MenuProps) {
+export function Menu({onAddToCart, products}: MenuProps) {
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
 
